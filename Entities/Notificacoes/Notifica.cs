97c6e164f40c1b4d6caf6615837cad71fd862108
samuelsp <samp_81@yahoo.com.br>
2023-03-09 09:38:07 -0300

    Entidades

---
+++ Entities/Notificacoes/Notifica.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Notificacoes
+{
+    public class Notifica
+    {
+        public Notifica()
+        {
+            notificacoes = new List<Notifica>();
+        }
+
+        [NotMapped]
+        public string? NomePropriedade { get; set; }
+
+        [NotMapped]
+        public string? mensagem { get; set; }
+
+        [NotMapped]
+        public List<Notifica> notificacoes { get; set; }
+
+        public bool ValidarPropriedadeString(string valor, string nomePropriedade)
+        {
+            if (string.IsNullOrWhiteSpace(valor) || string.IsNullOrWhiteSpace(nomePropriedade))
+            {
+                notificacoes.Add(new Notifica
+                {
+                    mensagem = "Campo obrigatório",
+                    NomePropriedade = nomePropriedade
+                });
+
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool ValidarPropriedadeInt(int valor, string nomePropriedade)
+        {
+            if (valor < 1 || string.IsNullOrWhiteSpace(nomePropriedade))
+            {
+                notificacoes.Add(new Notifica
+                {
+                    mensagem = "Campo obrigatório",
+                    NomePropriedade = nomePropriedade
+                });
+
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
